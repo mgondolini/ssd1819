b259b0c789eb0c8fa968a5ac18272881307dfe66
Monica Gondolini <monica.gondolini@studio.unibo.it>
2019-02-08 13:38:13 +0100

    Initial commit.

---
+++ WebApplication1/WebApplication1/Models/Model.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Common;
+using Newtonsoft.Json;
+using System.IO;
+using System.Data;
+
+namespace WebApplication1.Models
+{
+    public class Model
+    {
+        public delegate void viewEventHandler(object sender, string textToWrite);
+
+        private string pathjson = @"C:\Users\monyg\source\repos\WebApplication1\WebApplication1\App_Data\toy.json";
+
+        private GAPinstance GAP;
+        private BasicHeu basicHeu;
+
+        public GAPinstance readGAPInstance(string path)
+        {
+            StreamReader fin;
+
+            try
+            {
+                fin = new StreamReader(path);
+                string jstring = fin.ReadToEnd();
+                System.Diagnostics.Debug.WriteLine(jstring);
+                GAP = JsonConvert.DeserializeObject<GAPinstance>(jstring);
+                fin.Close();
+
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Error " + ex.Message);
+            }
+            return GAP;
+        }
+
+        public string readSerie(string connString, string factory)
+        {
+            System.Diagnostics.Debug.WriteLine("connstring " + connString + " factory" + factory);
+            DbProviderFactory dbFactory = DbProviderFactories.GetFactory(factory);
+
+            string res = "";
+
+            using (DbConnection conn = dbFactory.CreateConnection())
+            {
+                try
+                {
+                    conn.ConnectionString = connString;
+                    conn.Open();
+                    DbCommand com = conn.CreateCommand();
+                    com.CommandText = "select time from serie";
+
+                    DbDataReader reader = com.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        System.Diagnostics.Debug.WriteLine(reader["time"] + "");
+                        res = reader["time"] + "";
+                        goto l0;
+                    }
+                        
+
+                    reader.Close();
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine( "[dataReader] Error: " + ex.Message);
+                    res = "[dataReader] Error: " + ex.Message;
+                }
+                finally
+                {
+                    if (conn.State == ConnectionState.Open) conn.Close();
+                }
+            }
+            l0:
+            return res;
+        }
+
+        public int constructSolution(GAPinstance instance)
+        {
+            basicHeu = new BasicHeu(instance);
+            return basicHeu.constructFirtsSol();
+        }
+
+        public int opt10(GAPinstance instance)
+        {
+            basicHeu = new BasicHeu(instance);
+            basicHeu.constructFirtsSol();
+            return basicHeu.opt10(GAP.cost);
+        }
+
+        public int simulatedAnnealing(GAPinstance instance)
+        {
+            basicHeu = new BasicHeu(instance);
+            return basicHeu.simulatedAnnealing();
+        }
+
+    }
+}
---
+++ WebApplication1/WebApplication1/script.js
@@ -0,0 +1,116 @@
+﻿var GAPinstance;
+
+function constructSolution() {
+    GAPinstance = $("#gap_name").children("option:selected").val();
+    $.ajax({
+        url: "api/Clienti/constructSolution/" + GAPinstance,
+        type: "GET",
+        contentType: "application/json",
+        success: function (result) {
+            $("#solution").text("Solution: " + result);
+        },
+        error: function (xhr, status, p3, p4) {
+            var err = "Error " + " " + status + " " + p3;
+            if (xhr.responseText && xhr.responseText[0] == "{")
+                err = JSON.parse(xhr.responseText).message;
+            alert(err);
+        }
+    });
+    event.preventDefault();
+}
+
+function optimization() {
+    GAPinstance = $("#gap_name").children("option:selected").val();
+    $.ajax({
+        url: "api/Clienti/optimization/" + GAPinstance,
+        type: "GET",
+        contentType: "application/json",
+        success: function (result) {
+            $("#optimization").text("Optimization: " + result);
+        },
+        error: function (xhr, status, p3, p4) {
+            var err = "Error " + " " + status + " " + p3;
+            if (xhr.responseText && xhr.responseText[0] == "{")
+                err = JSON.parse(xhr.responseText).message;
+            alert(err+ "we");
+        }
+    });
+    event.preventDefault();
+}
+
+function simulatedAnnealing() {
+    GAPinstance = $("#gap_name").children("option:selected").val();
+    $.ajax({
+        url: "api/Clienti/simulatedAnnealing/" + GAPinstance,
+        type: "GET",
+        contentType: "application/json",
+        success: function (result) {
+            $("#simulatedAnnealing").text("Simulated Annealing: " + result);
+        },
+        error: function (xhr, status, p3, p4) {
+            var err = "Error " + " " + status + " " + p3;
+            if (xhr.responseText && xhr.responseText[0] == "{")
+                err = JSON.parse(xhr.responseText).message;
+            alert(err);
+        }
+    });
+    event.preventDefault();
+}
+
+
+function readSerie() {
+    $.ajax({
+        url: "api/Clienti/readSerie",
+        type: "GET",
+        contentType: "application/json",
+        success: function (result) {
+            alert(result)
+        },
+        error: function (xhr, status, p3, p4) {
+            var err = "Error " + " " + status + " " + p3;
+            if (xhr.responseText && xhr.responseText[0] == "{")
+                err = JSON.parse(xhr.responseText).message;
+            alert(err);
+        }
+    });
+}
+
+
+function postSomething(id, dato) {
+    var options = {};
+    options.url = "/api/Clienti/postSomething";
+    options.type = "POST";
+    options.data = "{\"id\":" + id + ",\"dato\":\"" + dato + "\"}";
+    options.dataType = "json";
+    options.contentType = "application/json";
+    options.success = function (msg) {
+        alert(msg);
+    };
+    options.error = function (err) {
+        alert(err.statusText);
+    };
+    $.ajax(options); // chiamata http
+}
+
+
+function readGAPinstance() {
+    GAPinstance = $("#gap_name").children("option:selected").val();
+    $.ajax({
+        url: "api/Clienti/readGAPinstance/" + GAPinstance,
+        type: "GET",
+        contentType: "application/json",
+        data: "",
+        success: function (result) {
+            $("#solution").text("Solution: " + result);
+        },
+        error: function (xhr, status, p3, p4) {
+            var err = "Error " + " " + status + " " + p3;
+            if (xhr.responseText && xhr.responseText[0] == "{")
+                err = JSON.parse(xhr.responseText).message;
+            alert(err);
+        }
+    });
+    event.preventDefault();
+}
+
+
